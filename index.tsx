2864981aec17122835e0e1b577dae837e3034996
RAHULMANI0407 <mightykong0407@gmail.com>
2025-12-31 08:44:40 +0530

    Update index.tsx

--- index.tsx
+++ index.tsx
@@ -1,15 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
 
-const root = document.getElementById("root");
-
-if (!root) {
-  throw new Error("Root not found");
-}
-
-ReactDOM.createRoot(root).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <div style={{ color: "white", padding: 20 }}>
+    REACT IS WORKING
+  </div>
 );
